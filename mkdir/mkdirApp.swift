86ea4abbf03412cda301fcd409eddeb394b28e29
mohd-kasif <kashifmd123@gmail.com>
2023-11-08 15:57:59 +0530

    Initial Commit

---
+++ mkdir/mkdirApp.swift
@@ -0,0 +1,17 @@
+//
+//  mkdirApp.swift
+//  mkdir
+//
+//  Created by Apple on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct mkdirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
